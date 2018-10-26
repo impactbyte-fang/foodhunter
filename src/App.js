70b67b1040a6217c186139002d7c4df00e682823
M Haidar Hanif <git@mhaidarhanif.com>
2018-10-26 15:02:42 +0700

    Delete CSS and logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
 
 import ListOfFoods from "./ListOfFoods.js"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header>
           <h1>FoodHunter</h1>
-          <ol>
-            <ListOfFoods />
-          </ol>
         </header>
+        <section>
+          <ListOfFoods />
+        </section>
       </div>
     )
   }
